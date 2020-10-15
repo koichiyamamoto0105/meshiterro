bc6ea8d560afdd60136bddd3f05256791777a54f
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-10-15 12:11:21 +0000

    [en milieue 4-4]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b1f5351a7d93ea2350be6ec14a014af35f682f2212744b27837ef1203f231c729af5998ea03d9f1c64bbdfced11fc7ee7218360056d134981e9036508bcec67c'
